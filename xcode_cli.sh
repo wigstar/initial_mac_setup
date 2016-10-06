df44793a8b8bf5281a920f82b13d024fc355a654
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-06 06:21:09 +0100

    waits for xcode cli complete download

--- xcode_cli.sh
+++ xcode_cli.sh
@@ -2,6 +2,8 @@
 
 # Install XCode Command Line Tools with AppleScript
 # Note: License Agreement Acceptance is currently hard coded as tabs
+echo "Downloading XCode Command Line Tools..."; echo
+
 xcode-select --install
 sleep 1
 osascript <<EOD
@@ -14,3 +16,15 @@ osascript <<EOD
     end tell
   end tell
 EOD
+
+
+# Wait for download to finish
+PROCESS_PID=`/bin/ps -fu $USER| grep "Install Command Line Developer Tools" | grep -v "grep" | awk '{print $2}'`
+while kill -0 $PROCESS_PID >/dev/null 2>&1
+  do
+    echo "Downloading..."
+    sleep 10
+done
+
+
+echo "XCode Command Line Tools Download Complete"; echo
