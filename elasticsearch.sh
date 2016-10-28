2871d6614e3f03f7db68e9224d0a805d29f0b675
Tiffanie Chia <tiffaniechia@Tiffanies-MacBook-Pro.local>
2016-10-28 17:52:31 +0100

    adds python stuff

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -4,7 +4,8 @@
 echo "Downloading Elasticsearch into custom project folder..."; echo
 
 cd ~/projects
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.0.zip
-unzip elasticsearch-5.0.0.zip
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.4.0/elasticsearch-2.4.0.zip
+unzip elasticsearch-2.4.0.zip
+~/projects/elasticsearch-2.4.0/bin/plugin install mobz/elasticsearch-head
 
 echo "Elasticsearch Download Complete"; echo
